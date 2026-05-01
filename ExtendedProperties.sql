9b35f8ce93e3cca4e120d13cca428bbe5c7cb2f2
Filipe Mello-DBA <filipemelloti@hotmail.com>
2026-05-01 05:48:33 -0300

    teste

--- ExtendedProperties.sql
+++ ExtendedProperties.sql
@@ -3,8 +3,3 @@ EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Primary fil
 
 GO
 
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'AdventureWorks 2016 Sample OLTP Database';
-
-
-GO
-
